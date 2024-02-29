8a9f9b0cf0d0267ecc1cf008760b895fdbf8f5f7
Ayan2002Maity <ayanmaity943@gmail.com>
2024-02-29 20:49:09 +0530

    react element

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,38 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+// react element by programer . it will not work
+// const reactElement ={
+//   type : 'a',
+//   props :{
+
+//       href : "https://google.com",
+//       target : "_blank"
+//   },
+//   children : "Click me to visit google"
+// }
+
+const newElement = "i am happy"
+
+//react element creation according to react format
+const reactElement = React.createElement(
+  'a',
+  {
+    href:'https://google.com' , target : "_blank"
+  } ,
+  'click me to visti google'
+  , newElement // ---> evaluated expression
+
+)
+
+
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank">visit google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <App />
+
+    // anotherElement
+    reactElement
 )
